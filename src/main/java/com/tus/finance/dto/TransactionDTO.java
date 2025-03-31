ede2d01e464dcb6a64811da2d3c8c9aca20a8d76
Velvizhi <A00325399@student.tus.ie>
2025-03-31 12:50:30 +0100

    Made some changes

--- src/main/java/com/tus/finance/dto/TransactionDTO.java
+++ src/main/java/com/tus/finance/dto/TransactionDTO.java
@@ -1,10 +1,5 @@
 package com.tus.finance.dto;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class TransactionDTO {
     private Long id;
     private String description;
